d6b79d8cc0d47b878effffffc1e2442d8129f7bf
CJ Kenney <ckenney422@gmail.com>
2019-02-14 11:48:27 -0600

    call for data feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -7,8 +7,33 @@ const StyledFeed = styled.div`
 `;
 
 class Feed extends Component {
+  state = { messages: '' };
+
+  componentDidMount() {
+    fetch('https://pure-plateau-88745.herokuapp.com/')
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        console.log(data);
+
+        let feed = data.map((item, index) => {
+          return (
+            <p>
+              {item.date} - {item.message}
+            </p>
+          );
+        });
+        this.setState({ messages: feed });
+        console.log('state', this.state.messages);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
-    return <StyledFeed>This is the feed</StyledFeed>;
+    return <StyledFeed>This is the feed {this.state.messages}</StyledFeed>;
   }
 }
 
